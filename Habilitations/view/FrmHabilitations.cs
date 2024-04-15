2787c345b7ac3bcd141d4b39da22d19ef1c4ffab
Théotim <Thotim@MSI>
2024-04-16 00:52:26 +0200

    Ajoutez des fichiers projet.

---
+++ Habilitations/view/FrmHabilitations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Habilitations.view
+{
+    public partial class FrmHabilitations : Form
+    {
+        public FrmHabilitations()
+        {
+            InitializeComponent();
+        }
+    }
+}
